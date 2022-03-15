adc9d0c8e5938315fc4fb6615fd5e74084ec43fa
James Morse <james.morse@arm.com>
2022-03-15 18:24:12 +0000

    KVM: arm64: Add templates for BHB mitigation sequences
    KVM writes the Spectre-v2 mitigation template at the beginning of each
    vector when a CPU requires a specific sequence to run.
    
    Because the template is copied, it can not be modified by the alternatives
    at runtime.
    
    Add templates for calling ARCH_WORKAROUND_3 and one for each value of K
    in the brancy-loop. Instead of adding dummy functions for 'fn', which would
    disable the Spectre-v2 mitigation, add template_start to indicate that a
    template (and which one) is in use. Finally add a copy of
    install_bp_hardening_cb() that is able to install these.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -55,9 +55,8 @@
 #define ARM64_WORKAROUND_CAVIUM_TX2_219_TVM	45
 #define ARM64_WORKAROUND_CAVIUM_TX2_219_PRFM	46
 #define ARM64_WORKAROUND_1542419		47
-#define ARM64_HAS_AMU_EXTN			48
+#define ARM64_SPECTRE_BHB			48
 
-/* kabi: reserve 49 - 62 for future cpu capabilities */
-#define ARM64_NCAPS				62
+#define ARM64_NCAPS				49
 
 #endif /* __ASM_CPUCAPS_H */
